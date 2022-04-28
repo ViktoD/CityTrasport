9a3af252bd747fcd5121d2c295f18e62ee37fecc
ViktoD <90144863+ViktoD@users.noreply.github.com>
2022-04-28 18:29:01 +0300

    Update connect.php

--- views/connect.php
+++ views/connect.php
@@ -2,7 +2,7 @@
 
     try{
 
-        $PDOconn = new PDO("pgsql:host=localhost;port=5432;dbname=CityTransport;","postgres","83159607");
+        $PDOconn = new PDO("pgsql:host=ec2-63-35-156-160.eu-west-1.compute.amazonaws.com;port=5432;dbname=d6mtmp12tef8k5;","wymdkhigtzozej","151e0be2a4d9bebc89c55517142bc9003db6a5e48ec45c01cfef0a49485d4098");
     }catch(Exception $e){
         echo $e;
     }
